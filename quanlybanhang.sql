75fe386d9adfd739bf3da9f7e64728b009a2e7e5
ThuyHuong1702 <dothuyhuong90@gmail.com>
2025-03-06 04:35:40 +0700

    Update quanlybanhang.sql

--- quanlybanhang.sql
+++ quanlybanhang.sql
@@ -317,3 +317,356 @@ FROM products AS p
 LEFT JOIN order_details AS od ON p.product_id = od.product_id
 GROUP BY p.product_id, p.product_name, p.quantity_in_stock
 ORDER BY remaining_stock DESC;
+
+
+-- 30
+SELECT 
+    o.order_id AS `Mã đơn hàng`,
+    GROUP_CONCAT(p.product_name SEPARATOR ', ') AS `Danh sách mặt hàng`,
+    SUM(od.quantity * od.selling_price - od.discount_amount*od.quantity) AS `Tổng số tiền`
+FROM 
+    orders o
+JOIN 
+    order_details od ON o.order_id = od.order_id
+JOIN 
+    products p ON od.product_id = p.product_id
+GROUP BY 
+    o.order_id
+ORDER BY 
+    o.order_id;
+
+-- 31
+SELECT 
+    pt.product_type_id AS `Mã loại hàng`,
+    pt.product_type_name AS `Tên loại hàng`,
+    GROUP_CONCAT(p.product_name SEPARATOR ', ') AS `Danh sách mặt hàng`,
+    SUM(p.quantity) AS `Tổng số lượng của loại hàng`,
+    (SELECT SUM(quantity) FROM products) AS `Tổng số lượng của tất cả các mặt hàng`
+FROM 
+    product_types pt
+JOIN 
+    products p ON pt.product_type_id = p.product_type_id
+GROUP BY 
+    pt.product_type_id, pt.product_type_name
+ORDER BY 
+    pt.product_type_id;
+-- 32
+
+SELECT 
+    p.product_id AS `Mã hàng`,
+    p.product_name AS `Tên hàng`,
+    SUM(CASE WHEN MONTH(o.order_date) = 1 THEN od.quantity ELSE 0 END) AS `Tháng 1`,
+    SUM(CASE WHEN MONTH(o.order_date) = 2 THEN od.quantity ELSE 0 END) AS `Tháng 2`,
+    SUM(CASE WHEN MONTH(o.order_date) = 3 THEN od.quantity ELSE 0 END) AS `Tháng 3`,
+    SUM(CASE WHEN MONTH(o.order_date) = 4 THEN od.quantity ELSE 0 END) AS `Tháng 4`,
+    SUM(CASE WHEN MONTH(o.order_date) = 5 THEN od.quantity ELSE 0 END) AS `Tháng 5`,
+    SUM(CASE WHEN MONTH(o.order_date) = 6 THEN od.quantity ELSE 0 END) AS `Tháng 6`,
+    SUM(CASE WHEN MONTH(o.order_date) = 7 THEN od.quantity ELSE 0 END) AS `Tháng 7`,
+    SUM(CASE WHEN MONTH(o.order_date) = 8 THEN od.quantity ELSE 0 END) AS `Tháng 8`,
+    SUM(CASE WHEN MONTH(o.order_date) = 9 THEN od.quantity ELSE 0 END) AS `Tháng 9`,
+    SUM(CASE WHEN MONTH(o.order_date) = 10 THEN od.quantity ELSE 0 END) AS `Tháng 10`,
+    SUM(CASE WHEN MONTH(o.order_date) = 11 THEN od.quantity ELSE 0 END) AS `Tháng 11`,
+    SUM(CASE WHEN MONTH(o.order_date) = 12 THEN od.quantity ELSE 0 END) AS `Tháng 12`,
+    SUM(od.quantity) AS `Tổng cả năm`
+FROM 
+    order_details od
+JOIN 
+    orders o ON od.order_id = o.order_id
+JOIN 
+    products p ON od.product_id = p.product_id
+WHERE 
+    YEAR(o.order_date) = 2025
+GROUP BY 
+    p.product_id, p.product_name
+ORDER BY 
+    p.product_id;
+
+
+-- proc
+-- 1. Tạo thủ tục lưu trữ để thông qua thủ tục này có thể bổ sung thêm một bản ghi mới cho bảng `products` (thủ tục phải thực hiện kiểm tra tính hợp lệ của dữ liệu cần bổ úng: không trùng khoá chính v đa bảo toàn vẹn tham chiếu)
+DELIMITER //
+CREATE PROCEDURE add_product(
+							IN `p_product_id` BIGINT,
+							IN `p_company_id` BIGINT,
+							IN `p_product_type_id` BIGINT,
+							IN `p_product_name` VARCHAR(30),
+							IN `p_quantity` INT,
+							IN `p_unit` VARCHAR(10),
+							IN `p_price` NUMERIC(10, 2)
+						)
+BEGIN
+	IF EXISTS ( SELECT 1 FROM products AS p WHERE p.product_id = p_product_id) THEN
+		SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Lỗi: product_id đã tồn tại!';
+   END IF;
+   
+   IF NOT EXISTS ( SELECT 1 FROM suppliers AS s WHERE s.company_id = p_company_id ) THEN 
+		SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Lỗi: company_id không tồn tại tron bảng suppliers!';
+   END IF;
+   
+   IF NOT EXISTS ( SELECT 1 FROM product_types AS pt WHERE pt.product_type_id = p_product_type_id ) THEN
+   	SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Lỗi: product_type_id không tồn tại tron bảng product_types!';
+   END IF;
+   
+	INSERT INTO products(product_id, company_id, product_type_id, product_name, quantity, unit, price)
+	VALUES (p_product_id, p_company_id, p_product_type_id, p_product_name, p_quantity, p_unit, p_price);
+END //
+DELIMITER ;
+
+CALL add_product(24, 11, 1, 'Sửa hộp XYZ', 10, 'Hộp', 4000);
+
+-- 2. Tạo thủ tục lưu trữ có chức năng thông kế tổng số lượng hàng bán được của một mặt hàng có mã bất kỳ (mã hàng cần thống kê là tham số)
+DELIMITER //
+CREATE PROCEDURE report_product(
+								IN `r_product_id` BIGINT
+							)
+BEGIN 
+	SELECT product_id, SUM(quantity)
+	FROM order_details 
+	WHERE product_id = r_product_id
+	GROUP BY product_id;
+END //
+DELIMITER;
+
+DROP PROCEDURE IF EXISTS report_product;
+
+CALL report_product(3)
+
+-- 3. Viết hàm trả về một bảng trong đó cho biết tôgnr số lượng hàng bán của mỗi mặt hàng. Sử dụng ha này thống kê xem tổng số lượng hàng (hiện có và đang bán) của mỗi mặt hàng là bao nhiêu
+
+DELIMITER //
+CREATE PROCEDURE report_product_sale()
+BEGIN
+	SELECT p.product_id, p.product_name, COALESCE(SUM(od.quantity), 0) AS total_sold, (p.quantity - COALESCE(SUM(od.quantity), 0)) AS remaining_product
+	FROM products AS p LEFT JOIN order_details AS od ON p.product_id = od.product_id
+	GROUP BY p.product_id, p.product_name;
+END //
+DELIMITER;
+
+CALL report_product_sale();
+
+
+-- trigger 1. Vết trigger cho bảng `order_details` theo yêu cầu sau:
+-- - Khi một bản ghi mới được bổ sung vào bảng này thì giảm số lượng hàng hiện tại có nếu số lượng hàng hiện có lớn hơn hoặc bằng số lượng hàng được bán ra. Ngược lại thì huỷ bỏ thao tác bổ sung.
+-- - Khi cập nhật số lượng hàng được bán, kiểm tra số lượng hàng được cập nhật lại có phù hợp hay không (số lượng hàng bán ra không được vượt qu số lượng hàng hiện có và không được nhỏ hơn 1). Nếu dữ liệu hợp lệ thì giảm (hoặc tăng) số lượng hiện có trong công ty, ngược lại thì huỷ bỏ thao tác cập nhật.
+
+DELIMITER //
+CREATE TRIGGER insert_order_detail
+BEFORE INSERT ON order_details
+FOR EACH ROW
+BEGIN
+	DECLARE available_quantity INT;
+	SELECT quantity INTO available_quantity
+	FROM products
+	WHERE product_id = NEW.product_id;
+	
+	IF NEW.quantity > available_quantity THEN
+		SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Lỗi: số lượng vượt quá số lượng sản phẩm hiện có!';
+   ELSE
+   	UPDATE products
+   	SET quantity = quantity - NEW.quantity
+   	WHERE product_id = NEW.product_id;
+   END IF;
+END //
+DELIMITER;
+
+INSERT INTO order_details (order_id, product_id, selling_price, quantity, discount_amount)
+VALUES (9, 2, 1000, 2, 0);
+
+-- - Khi cập nhật số lượng hàng được bán, kiểm tra số lượng hàng được cập nhật lại có phù hợp hay không (số lượng hàng bán ra không được vượt qu số lượng hàng hiện có và không được nhỏ hơn 1). Nếu dữ liệu hợp lệ thì giảm (hoặc tăng) số lượng hiện có trong công ty, ngược lại thì huỷ bỏ thao tác cập nhật.
+
+DELIMITER //
+CREATE TRIGGER check_quantity_sales 
+BEFORE UPDATE ON order_details
+FOR EACH ROW
+BEGIN
+	DECLARE available_quantity BIGINT;
+	DECLARE deference_quantity BIGINT;
+	
+	SELECT quantity INTO available_quantity
+	FROM products
+	WHERE product_id = OLD.product_id;
+	
+	IF NEW.quantity > available_quantity + OLD.quantity THEN
+		SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Lỗi: số lượng vượt quá số lượng sản phẩm hiện có!';
+   ELSEIF NEW.quantity < 1 THEN
+   	SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Lỗi: số lượng sản phẩm phải lớn hơn 1!';
+   ELSE
+   	SET deference_quantity = NEW.quantity - OLD.quantity;
+   	UPDATE products
+   	SET quantity = quantity - deference_quantity
+   	WHERE product_id = OLD.product_id;
+   END IF;
+END //
+DELIMITER;
+
+DROP TRIGGER IF EXISTS check_quantity_sales;
+
+UPDATE order_details 
+SET quantity = 3
+WHERE order_id = 1 AND product_id = 1;
+
+UPDATE order_details 
+SET quantity = 20
+WHERE order_id = 1 AND product_id = 1;
+
+UPDATE order_details 
+SET quantity = 0
+WHERE order_id = 1 AND product_id = 1;
+
+-- Viết trigger cho bảng `order_details` sao cho chỉ chấp nhận giá hàng bán ra phải nhỏ hơn hoặc bằng giá gốc (giá của mặt hàng trong bảng `products`)
+
+DELIMITER //
+CREATE TRIGGER check_price_sales_insert
+BEFORE INSERT ON order_details
+FOR EACH ROW
+BEGIN
+	DECLARE price_origin DECIMAL(10,2);
+	
+	SELECT price INTO price_origin
+	FROM products
+	WHERE product_id = NEW.product_id;
+	
+	IF NEW.selling_price > price_origin THEN
+		SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Lỗi: giá hàng bán ra phải nhỏ hơn hoặc bằng giá gốc!';
+   END IF;
+END //
+DELIMITER
+
+DROP TRIGGER IF EXISTS check_price_sales;
+
+
+INSERT INTO order_details(order_id, product_id, selling_price, quantity, discount_amount)
+VALUES (8, 3, 66600000.0, 2, 0);
+
+
+
+DELIMITER //
+CREATE TRIGGER check_price_sales_update
+BEFORE UPDATE ON order_details
+FOR EACH ROW
+BEGIN
+    DECLARE price_origin DECIMAL(10,2);
+
+    SELECT price INTO price_origin
+    FROM products
+    WHERE product_id = NEW.product_id;
+
+    IF NEW.selling_price > price_origin THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Lỗi: Giá hàng bán ra phải nhỏ hơn hoặc bằng giá gốc!';
+    END IF;
+END //
+
+DELIMITER ;
+
+UPDATE order_details 
+SET selling_price = 50000.0
+WHERE order_id = 1 AND product_id = 1;
+
+
+-- UPDATE 
+-- 1. Cập nhật lại giá thị trường NGAYCHUYENHANG của những bản ghi có NGAYCHUYENHANG chưa xác định (NULL) trong bảng DONDATHANG bằng với giá trị của trường NGAYDATHANG
+UPDATE orders 
+SET delivery_date = order_date
+WHERE delivery_date IS NULL;
+
+-- 3. Cập nhật giá trị của trường NOIGIAOHANG trong bảng DONDATHANG bằng địa chỉ của khách hàng đối với những đơn đặt hàng chưa xác định được nơi giao hàng (giá trị trường NOIGIAOHANG bằng NULL).
+UPDATE orders 
+JOIN customers ON orders.customer_id = customers.customer_id
+SET orders.shipping_address = customers.address
+WHERE orders.shipping_address IS NULL;
+
+-- 4. Cập nhật lại dữ liệu trong bảng KHACHHANG sao cho nếu tên công ty và tên giao dịch của khách hàng trùng với tên công ty và tên giao dịch của một nhà cung cấp nào đó thì địa chỉ, điện thoại, fax và e-mail phải giống nhau.
+UPDATE customers c
+JOIN suppliers s ON c.company_name = s.company_name 
+                AND c.trading_name = s.trading_name
+SET c.address = s.address,
+    c.phone = s.phone,
+    c.fax = s.fax,
+    c.email = s.email;
+-- 5. Tăng lương lên gấp rưỡi cho những nhân viên bán được số lượng hàng nhiều hơn 100 trong năm 2003.
+UPDATE employees e
+SET e.base_salary = e.base_salary * 1.5
+WHERE e.employee_id IN (
+    SELECT od.order_id
+    FROM orders o
+    JOIN order_details od ON o.order_id = od.order_id
+    WHERE YEAR(o.order_date) = 2003
+    GROUP BY o.employee_id
+    HAVING SUM(od.quantity) > 100
+);
+
+-- 6. Tăng phụ cấp lên bằng 50% lương cho những nhân viên bán được hàng nhiều nhất.
+UPDATE employees e
+SET e.allowance = e.base_salary * 0.5
+WHERE e.employee_id IN (
+    SELECT s.employee_id
+    FROM (
+        SELECT o.employee_id, SUM(od.quantity) AS total_quantity
+        FROM orders o JOIN order_details od ON o.order_id = od.order_id
+        GROUP BY o.employee_id
+        HAVING SUM(od.quantity) = (
+            SELECT MAX(t.total_quantity)
+            FROM (
+                SELECT SUM(od.quantity) AS total_quantity
+                FROM orders o
+                JOIN order_details od ON o.order_id = od.order_id
+                GROUP BY o.employee_id
+            ) AS t
+        )
+    ) AS s
+);
+
+-- 7. Giảm 25% lương của những nhân viên trong năm 2003 không lập được bất kỳ đơn đặt hàng nào.
+UPDATE employees e
+SET e.base_salary = e.base_salary * 0.75
+WHERE e.employee_id NOT IN (
+    SELECT DISTINCT o.employee_id
+    FROM orders o
+    WHERE YEAR(o.order_date) = 2003
+);
+
+-- 8. Giả sử trong bảng DONDATHANG có thêm trường SOTIEN cho biết số tiền mà khách hàng phải trả trong mỗi đơn đặt hàng. Hãy tính giá trị cho trường này.
+
+UPDATE orders o
+SET SOTIEN = (
+    SELECT SUM(od.selling_price * od.quantity - od.discount_amount * od.quantity)
+    FROM order_details od
+    WHERE od.order_id = o.order_id
+);
+
+-- DELETE
+
+-- 1. Xoá khỏi bảng NHANVIEN những nhân viên đã làm việc trong công ty quá 40 năm.
+DELETE FROM employees
+WHERE TIMESTAMPDIFF(YEAR, start_date, CURDATE()) > 40;
+
+-- 2. Xoá những đơn đặt hàng trước năm 2000 ra khỏi cơ sở dữ liệu.
+
+DELETE FROM orders
+WHERE YEAR(order_date) < 2000;
+
+-- 3. Xoá khỏi bảng LOAIHANG những loại hàng hiện không có mặt hàng.
+
+DELETE FROM product_types
+WHERE product_type_id NOT IN (SELECT DISTINCT product_type_id FROM products);
+
+-- 4 Xoá khỏi bảng KHACHHANG những khách hàng hiện không có bất kỳ đơn đặt hàng nào cho công ty.
+
+DELETE FROM customers
+WHERE customer_id NOT IN (SELECT DISTINCT customer_id FROM orders);
+
+SELECT * FROM orders
+
+-- 5. Xoá khỏi bảng MATHANG những mặt hàng có số lượng bằng 0 và không được đặt mua trong bất kỳ đơn đặt hàng nào.
+
+DELETE FROM products
+WHERE quantity = 0
+AND product_id NOT IN (SELECT DISTINCT product_id FROM order_details);
